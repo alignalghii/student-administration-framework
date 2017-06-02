4cdaac15f6cdb806be74bfe066fe1a3cc7e7450f
Mark Endrey <ungazik@gmail.com>
2017-06-02 18:07:01 +0200

    First notion of controller classes and objects

--- router.php
+++ router.php
@@ -1,14 +1,21 @@
 <?php
 
 $routes = [
-	['GET', '/',                 'index',       ],
-	['GET', '/student',          'student_index'],
-	['GET', '/student/([0-9]+)', 'student_show' ]
+	['GET', '/',                 'DefaultController', 'index'],
+	['GET', '/student',          'StudentController', 'index'],
+	['GET', '/student/([0-9]+)', 'StudentController', 'show' ]
 ];
 
-function index()           {echo 'Root';}
-function student_index()   {echo 'List of all students';}
-function student_show($id) {echo "Show student #$id";}
+class DefaultController
+{
+	public function index() {echo 'Root';}
+}
+
+class StudentController
+{
+	public function index()   {echo 'List all users';}
+	public function show($id) {echo "Show student #$id";}
+}
 
 
 $uri    = $_SERVER['REQUEST_URI'];
@@ -17,13 +24,13 @@ $method = $_SERVER['REQUEST_METHOD'];
 $found = false;
 foreach ($routes as $route) {
 	if (!$found) {
-		list($routeMethod, $uriPattern, $action) = $route;
+		list($routeMethod, $uriPattern, $controller, $action) = $route;
 		if ($method == $routeMethod) {
 			$uriPattern = "!^$uriPattern$!";
 			$matched = preg_match($uriPattern, $uri, $match);
 			if ($matched) {
 				array_shift($match);
-				call_user_func_array($action, $match);
+				call_user_func_array([new $controller, $action], $match);
 				$found = true;
 			}
 		}
