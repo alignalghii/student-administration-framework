9d905399a808de99a56647e4e46acce7671ab4f0
Mark Endrey <ungazik@gmail.com>
2017-06-02 17:27:13 +0200

    A real router, with method checks and regexpable URI parameters and method checks

--- router.php
+++ router.php
@@ -1,16 +1,36 @@
 <?php
 
-$uri = $_SERVER['REQUEST_URI'];
-switch ($uri) {
-	case '/':
-		echo 'Root';
+$routes = [
+	['GET', '/',                 'index',       ],
+	['GET', '/student',          'student_index'],
+	['GET', '/student/([0-9]+)', 'student_show' ]
+];
+
+function index()           {echo 'Root';}
+function student_index()   {echo 'List of all students';}
+function student_show($id) {echo "Show student #$id";}
+
+
+$uri    = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
+
+$found = false;
+foreach ($routes as $route) {
+	if (!$found) {
+		list($routeMethod, $uriPattern, $action) = $route;
+		if ($method == $routeMethod) {
+			$uriPattern = "!^$uriPattern$!";
+			$matched = preg_match($uriPattern, $uri, $match);
+			if ($matched) {
+				array_shift($match);
+				call_user_func_array($action, $match);
+				$found = true;
+			}
+		}
+	} else {
 		break;
-	case '/student';
-		echo 'List of all students';
-		break;
-	case '/student/1';
-		echo 'Show first student';
-		break;
-	default:
-		echo "Resource not found at request URI $uri";
+	}
+}
+if (!$found) {
+	echo "No matchable route pattern found for `$method $uri`";
 }
