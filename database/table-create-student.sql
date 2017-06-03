35042cb582643f8c51f21695b73f36aaebf131c3
Mark Endrey <ungazik@gmail.com>
2017-06-03 14:58:33 +0200

    Beautify SQL scheme format

--- database/table-create-student.sql
+++ database/table-create-student.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `student` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`name` VARCHAR(255) NOT NULL,
-	`is_male` BOOLEAN NOT NULL,
+	`id`             INT          NOT NULL AUTO_INCREMENT,
+	`name`           VARCHAR(255) NOT NULL,
+	`is_male`        BOOLEAN      NOT NULL,
 	`place_of_birth` VARCHAR(255),
-	`date_of_birth` VARCHAR(255),
-	`email` VARCHAR(255) NOT NULL,
+	`date_of_birth`  DATE,
+	`email`          VARCHAR(255) NOT NULL,
 	PRIMARY KEY (`id`)
 );
--- database/table-create-student_study_group_membership.sql
+++ database/table-create-student_study_group_membership.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `student_study_group_membership` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`student_id` INT NOT NULL,
+	`id`             INT NOT NULL AUTO_INCREMENT,
+	`student_id`     INT NOT NULL,
 	`study_group_id` INT NOT NULL,
 	PRIMARY KEY (`id`),
-	FOREIGN KEY (`student_id`) REFERENCES `student` (`id`),
+	FOREIGN KEY (`student_id`)     REFERENCES `student`     (`id`),
 	FOREIGN KEY (`study_group_id`) REFERENCES `study_group` (`id`)
 );
