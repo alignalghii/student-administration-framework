2f665a8fe895b9b01335ccefb47f99b889c2ceb9
Mark Endrey <ungazik@gmail.com>
2017-06-05 08:41:56 +0200

    Beautify form scource view

--- app/View/Student/show.php
+++ app/View/Student/show.php
@@ -5,20 +5,25 @@ ob_start();
 		<h1><?php echo $title; ?></h1>
 		<form method="POST" action="<?php echo $action; ?>">
 			<?php if (!$isNew): ?><input type="hidden" name="id" value="<?php echo $id; ?>"/><?php endif; ?>
-			Name <input type="text" name="name" value="<?php echo $student['name']; ?>"/>
+			Name
+			<input type="text"       name="name"            value="<?php echo $student['name']; ?>"/>
 			<?php echo $validationErrors['name'] ?? ''; ?>
 			<br/>
-			Male <input type="checkbox" name="is_male"<?php if ($student['is_male']): ?>checked<?php endif; ?>/>
+			Male
+			<input type="checkbox"   name="is_male"<?php if ($student['is_male']): ?>checked<?php endif; ?>/>
 			<br/>
-			Place of birth <input type="text" name="place_of_birth" value="<?php echo $student['place_of_birth']; ?>"/>
+			Place of birth
+			<input type="text"       name="place_of_birth"  value="<?php echo $student['place_of_birth']; ?>"/>
 			<br/>
-			Date of birth <input type="text" name="date_of_birth" value="<?php echo $student['date_of_birth']; ?>"/>
+			Date of birth
+			<input type="text"       name="date_of_birth"   value="<?php echo $student['date_of_birth']; ?>"/>
 			<?php echo $validationErrors['date_of_birth'] ?? ''; ?>
 			<br/>
-			Email <input type="text" name="email" value="<?php echo $student['email']; ?>"/>
+			Email
+			<input type="text"       name="email"           value="<?php echo $student['email']; ?>"/>
 			<?php echo $validationErrors['email'] ?? ''; ?>
 			<br/>
-			<input type="submit" name="submit" value="Submit"/>
+			<input type="submit"     name="submit"          value="Submit"/>
 			<a href="<?php echo $action; ?>">Reset changes</a>
 		</form>
 <?php
