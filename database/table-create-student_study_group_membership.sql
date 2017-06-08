bcc3ffb3f1402f1b35ad54fc2e758aca39819187
Mark Endrey <ungazik@gmail.com>
2017-06-08 15:16:55 +0200

    Upond deleting a student, his/her group memberships get deleted automatically as well

--- database/table-create-student_study_group_membership.sql
+++ database/table-create-student_study_group_membership.sql
@@ -3,7 +3,7 @@ CREATE TABLE `student_study_group_membership` (
 	`student_id`     INT NOT NULL,
 	`study_group_id` INT NOT NULL,
 	PRIMARY KEY (`id`),
-	FOREIGN KEY (`student_id`)     REFERENCES `student`     (`id`),
-	FOREIGN KEY (`study_group_id`) REFERENCES `study_group` (`id`),
+	FOREIGN KEY (`student_id`)     REFERENCES `student`     (`id`) ON DELETE CASCADE,
+	FOREIGN KEY (`study_group_id`) REFERENCES `study_group` (`id`) ON DELETE CASCADE,
     UNIQUE KEY `unique_membership` (`student_id`, `study_group_id`)
 );
